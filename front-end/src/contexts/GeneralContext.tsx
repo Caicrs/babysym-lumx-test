89c5609255b341c87fd20c2655dc9dcb4e5d5c96
Caic Rocha <caicrs.contact@gmail.com>
2023-11-26 21:12:57 -0300

    Context - GeneralContext global modal added.

--- front-end/src/contexts/GeneralContext.tsx
+++ front-end/src/contexts/GeneralContext.tsx
@@ -1,24 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, createContext, useState } from "react";
+import Loading from "../components/loading";
+import ModalBase from "../components/modalBase";
 
 interface ContextProps {
   isOpenState: boolean;
+  modalOpened: ModalOpenedProps;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsModalOpened: React.Dispatch<React.SetStateAction<ModalOpenedProps>>;
+}
+export interface ModalOpenedProps {
+  opened: boolean;
+  body: React.JSX.Element;
 }
 
 export const GeneralContext = createContext<ContextProps>({} as ContextProps);
 
 export const GeneralProvider = ({ children }: any) => {
   const [isOpenState, setIsOpen] = useState(false);
+  const [loading, setIsLoading] = useState(false);
+  const [modalOpened, setIsModalOpened] = useState({
+    opened: true,
+    body: <h1>TEST 1</h1>,
+  });
 
   return (
     <GeneralContext.Provider
       value={{
+        modalOpened,
         isOpenState,
         setIsOpen,
+        setIsModalOpened,
       }}
     >
-      {children}
+      {loading ? <Loading /> : children}
+      <ModalBase
+        isOpen={modalOpened.opened}
+        onClose={() =>
+          setIsModalOpened({
+            opened: false,
+            body: <></>,
+          })
+        }
+      >
+        {modalOpened.body}
+      </ModalBase>
     </GeneralContext.Provider>
   );
 };
