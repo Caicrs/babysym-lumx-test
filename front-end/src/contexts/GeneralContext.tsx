b650c3b71954326aa4a80aae774841de0401dd91
Caic Rocha <caicrs.contact@gmail.com>
2023-11-28 02:45:01 -0300

    Context - Wallet metamask connection created.

--- front-end/src/contexts/GeneralContext.tsx
+++ front-end/src/contexts/GeneralContext.tsx
@@ -1,17 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 import Loading from "../components/loading";
 import ModalBase from "../components/modalBase";
+import SubmodalShareLink from "../components/submodal-share-link";
+import { useSDK } from "@metamask/sdk-react";
+import { ethers } from "ethers";
 
 interface ContextProps {
   isOpenState: boolean;
   modalOpened: ModalOpenedProps;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setIsModalOpened: React.Dispatch<React.SetStateAction<ModalOpenedProps>>;
+  wallet: WalletProps;
+  setWallet: React.Dispatch<React.SetStateAction<WalletProps>>;
+  connectWallet: () => void;
 }
 export interface ModalOpenedProps {
   opened: boolean;
   body: React.JSX.Element;
+}
+
+interface WalletProps {
+  address?: string;
+  balance?: string | null;
 }
 
 export const GeneralContext = createContext<ContextProps>({} as ContextProps);
@@ -20,13 +31,65 @@ export const GeneralProvider = ({ children }: any) => {
   const [isOpenState, setIsOpen] = useState(false);
   const [loading, setIsLoading] = useState(false);
   const [modalOpened, setIsModalOpened] = useState({
-    opened: true,
-    body: <h1>TEST 1</h1>,
+    opened: false,
+    title: "Share link",
+    body: <SubmodalShareLink />,
+  });
+
+  // usetstate for storing and retrieving wallet details
+  const [wallet, setWallet] = useState<WalletProps>({
+    address: "",
+    balance: null,
+  });
+
+  // Button handler button for handling a
+  // request event for metamask
+  const connectWallet = () => {
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+      // res[0] for fetching a first wallet
+      window.ethereum.request({ method: "eth_requestAccounts" }).then(
+        (res) => getbalance(res) // accountChangeHandler(res[0])
+      ).catch((err) => {console.log(err)});
+    } else {
+      setIsModalOpened({
+        title: "Alert",
+        opened: true,
+        body: <h3 style={{textAlign:'start'}}>Install the metamask extension</h3>,
+      })
+    }
+  };
+
+  // getbalance function for getting a balance in
+  // a right format with help of ethers
+  const getbalance = (address: any) => {
+    // Requesting balance method
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address[0], "latest"],
+      })
+      .then((balance: any) => {
+        // Setting balance
+        setWallet({
+          address: address[0],
+          balance: ethers.formatEther(balance),
+        });
+      });
+  };
+
+  useEffect(() => {
+    if (wallet.address == "") {
+      connectWallet();
+    }
   });
 
   return (
     <GeneralContext.Provider
       value={{
+        connectWallet,
+        wallet,
+        setWallet,
         modalOpened,
         isOpenState,
         setIsOpen,
@@ -36,8 +99,10 @@ export const GeneralProvider = ({ children }: any) => {
       {loading ? <Loading /> : children}
       <ModalBase
         isOpen={modalOpened.opened}
+        title={modalOpened.title}
         onClose={() =>
           setIsModalOpened({
+            title: "Only test 2",
             opened: false,
             body: <></>,
           })
