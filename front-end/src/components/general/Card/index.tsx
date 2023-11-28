5220c9f9daa1ac5cc0aae3be5a2443e8db4da04d
Caic Rocha <caicrs.contact@gmail.com>
2023-11-28 02:44:00 -0300

    Components - Card component finished.

--- front-end/src/components/general/Card/index.tsx
+++ front-end/src/components/general/Card/index.tsx
@@ -3,18 +3,29 @@
 import React from "react";
 import styled from "styled-components";
 
+interface CardContainerProps {
+  selected: boolean;
+}
+
 // Define the styled components
-const CardContainer = styled.div`
+const CardContainer = styled.div<CardContainerProps>`
   display: flex;
+  cursor: pointer;
+  ${(props) => (props.selected ? "outline: 2px solid #7879f1;" : "")};
   height: fit-content;
   flex-direction: column;
   border-radius: 16px;
   border-bottom: 1px solid rgba(238, 237, 237, 0.15);
   background: #2b2b2b;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    opacity: 0.8;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+  }
 `;
 
 const Image = styled.img`
-border-radius: 16px 16px 0 0;
+  border-radius: 16px 16px 0 0;
   width: 100%;
   height: 300px; /* Adjust the height as needed */
   object-fit: cover;
@@ -44,7 +55,9 @@ const Price = styled.p`
 interface ProductCardProps {
   imageSrc: string;
   title: string;
-  price: number;
+  price?: number;
+  toggleCardSelection?: () => void;
+  isSelected?: boolean;
 }
 
 // Create the functional component
@@ -52,13 +65,15 @@ const ProductCard: React.FC<ProductCardProps> = ({
   imageSrc,
   title,
   price,
+  toggleCardSelection,
+  isSelected,
 }) => {
   return (
-    <CardContainer>
+    <CardContainer onClick={toggleCardSelection} selected={isSelected}>
       <Image src={imageSrc} alt={title} />
       <ContentContainer>
         <Title>{title}</Title>
-        <Price>ETH {price.toFixed(2)}</Price>
+        {price && <Price>ETH {price.toFixed(2)}</Price>}
       </ContentContainer>
     </CardContainer>
   );
