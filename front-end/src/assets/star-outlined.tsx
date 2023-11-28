be03b294701879a37a3f99d3905e032660758c29
Caic Rocha <caicrs.contact@gmail.com>
2023-11-28 02:42:31 -0300

    Assets/Routes - New assets components and routes added.

---
+++ front-end/src/assets/star-outlined.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface StarProps {
+  isFilled: boolean;
+  props?: React.SVGProps<SVGSVGElement>;
+}
+
+const StarOutlined = ({
+  isFilled,
+  props,
+}: StarProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={36}
+    height={36}
+    fill={isFilled ? "yellow" : "none"}
+    stroke={isFilled ? "none" : "yellow"}
+    strokeWidth="1"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 2L9.8 8.5H2.5L8.5 12.5L6.5 19L12 15.4L17.5 19L15.5 12.5L21.5 8.5H14.2L12 2Z" />
+  </svg>
+);
+
+export default StarOutlined;
--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,18 +1,32 @@
-
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Homepage';
-import React from 'react';
-import Search from './pages/Search';
-import CollectionById from './pages/CollectionById';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage";
+import React from "react";
+import Search from "./pages/Search";
+import CollectionById from "./pages/CollectionById";
+import CustomizedCollectionById from "./pages/CustomizedCollectionById";
+import MyCustomCollection from "./pages/MyCustomCollection";
+import MyVotes from "./pages/MyVotes";
 
 const DefaultRoutes = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/search' element={<Search/>} />
-			<Route path='/collection/:id' element={<CollectionById/>} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/collection/:id" element={<CollectionById />} />
+      <Route
+        path="/customized-collection/:id"
+        element={<CustomizedCollectionById />}
+      />
+      <Route
+        path="/user/customized-collection"
+        element={<MyCustomCollection/>}
+      />
+      <Route
+        path="/user/votes"
+        element={<MyVotes/>}
+      />
+    </Routes>
+  );
 };
 
 export default DefaultRoutes;
