af030ab676106ea5d06f009dbe1482111bad21f2
Caic Rocha <caicrs.contact@gmail.com>
2023-11-29 12:54:21 -0300

    Geral - Swagger and new dependencies added

--- back-end/app.js
+++ back-end/app.js
@@ -4,9 +4,25 @@ import ipfsRoutes from './routes/ipfsRoutes';
 
 const app = express();
 
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'BabySym API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['src/index.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
 app.use('/ipfs', ipfsRoutes);
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Express server running at http://localhost:${port}`);
 });
